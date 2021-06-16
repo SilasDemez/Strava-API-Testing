164567dff27c46b5a38e2b46afec05768b0bc8d3
Silas Demez <silasdemez@gmail.com>
2021-06-16 14:56:06 +0200

    moved everything to a netlify webapp

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function authorize(){
 
     const client_id = process.env.CLIENT_ID;
     //const client_secret = process.env.CLIENT_SECRET;
-    const redirectUrl = 'http://localhost:63342/Strava-API-Testing/redirect.html'
+    const redirectUrl = 'https://stravatest.netlify.app/redirect.html'
 
 
     window.location = `https://www.strava.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUrl}&response_type=code&scope=activity:read_all`;
